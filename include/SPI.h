3324f74179f69b7f38f47a3324ffe474dc9954a8
Eduard-Cristian Bolos <ebolos@thoughtworks.com>
2017-11-18 23:38:14 +0100

    Use SPI Arduino implementation from esp8266 community

--- include/SPI.h
+++ include/SPI.h
@@ -1,89 +1,88 @@
 /*
- * Copyright (c) 2014-2017 Cesanta Software Limited
- * All rights reserved
- */
+  SPI.h - SPI library for esp8266
 
-#ifndef CS_FW_SRC_ARDUINO_SPI_H_
-#define CS_FW_SRC_ARDUINO_SPI_H_
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the esp8266 core for Arduino environment.
 
-#include "mgos_features.h"
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-#include <stdint.h>
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-#include "Arduino.h"
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#ifndef _SPI_H_INCLUDED
+#define _SPI_H_INCLUDED
 
-#include "mgos_spi.h"
+#include <Arduino.h>
+#include <stdlib.h>
 
-#ifndef LSBFIRST
-#define LSBFIRST 0
-#endif
-#ifndef MSBFIRST
-#define MSBFIRST 1
-#endif
+#define SPI_HAS_TRANSACTION
 
 // This defines are not representing the real Divider of the ESP8266
 // the Defines match to an AVR Arduino on 16MHz for better compatibility
-#define SPI_CLOCK_DIV2    0x00101001 //8 MHz
-#define SPI_CLOCK_DIV4    0x00241001 //4 MHz
-#define SPI_CLOCK_DIV8    0x004c1001 //2 MHz
-#define SPI_CLOCK_DIV16   0x009c1001 //1 MHz
-#define SPI_CLOCK_DIV32   0x013c1001 //500 KHz
-#define SPI_CLOCK_DIV64   0x027c1001 //250 KHz
-#define SPI_CLOCK_DIV128  0x04fc1001 //125 KHz
+#define SPI_CLOCK_DIV2 		0x00101001 //8 MHz
+#define SPI_CLOCK_DIV4 		0x00241001 //4 MHz
+#define SPI_CLOCK_DIV8 		0x004c1001 //2 MHz
+#define SPI_CLOCK_DIV16 	0x009c1001 //1 MHz
+#define SPI_CLOCK_DIV32 	0x013c1001 //500 KHz
+#define SPI_CLOCK_DIV64 	0x027c1001 //250 KHz
+#define SPI_CLOCK_DIV128 	0x04fc1001 //125 KHz
 
-#define SPI_MODE0 0x00
-#define SPI_MODE1 0x01
-#define SPI_MODE2 0x02
-#define SPI_MODE3 0x03
+const uint8_t SPI_MODE0 = 0x00; ///<  CPOL: 0  CPHA: 0
+const uint8_t SPI_MODE1 = 0x01; ///<  CPOL: 0  CPHA: 1
+const uint8_t SPI_MODE2 = 0x10; ///<  CPOL: 1  CPHA: 0
+const uint8_t SPI_MODE3 = 0x11; ///<  CPOL: 1  CPHA: 1
 
-#define SPI_HAS_TRANSACTION 1
-#define SPI_DEFAULT_FREQ 1000000
-
-struct SPISettings {
-  SPISettings();
-  SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode);
-  uint32_t clock;
-  uint8_t bit_order;
-  uint8_t mode;
+class SPISettings {
+    public:
+        SPISettings() :_clock(1000000), _bitOrder(LSBFIRST), _dataMode(SPI_MODE0){}
+        SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) :_clock(clock), _bitOrder(bitOrder), _dataMode(dataMode){}
+        uint32_t _clock;
+        uint8_t  _bitOrder;
+        uint8_t  _dataMode;
 };
 
-class SPIImpl {
- public:
-  SPIImpl();
-  ~SPIImpl();
-
-  /* Arduino interface */
-  void begin();
-  void end();
-
-  // void setHwCs(bool use);
-  void setBitOrder(uint8_t bitOrder);
-  void setDataMode(uint8_t dataMode);
-  void setFrequency(uint32_t freq);
-  void setClockDivider(uint32_t clockDiv);
-
-  void beginTransaction(SPISettings settings);
-  void endTransaction(void);
-
-  uint8_t transfer(uint8_t data);
-  uint16_t transfer16(uint16_t data);
-  uint32_t transfer32(uint32_t data);
-  void transfer(void *data, size_t count);
-  void transferBytes(const uint8_t *data, uint8_t *out, uint32_t size);
-  // void transferBits(uint32_t data, uint32_t * out, uint8_t bits);
-
-  void write(uint8_t data);
-  void write16(uint16_t data);
-  void write32(uint32_t data);
-  void writeBytes(const uint8_t *data, uint32_t size);
-
- private:
-  struct mgos_spi *spi_;
-  struct mgos_spi_txn txn_;
+class SPIClass {
+    public:
+        SPIClass();
+        bool pins(int8_t sck, int8_t miso, int8_t mosi, int8_t ss);
+        void begin();
+        void end();
+        void setHwCs(bool use);
+        void setBitOrder(uint8_t bitOrder);
+        void setDataMode(uint8_t dataMode);
+        void setFrequency(uint32_t freq);
+        void setClockDivider(uint32_t clockDiv);
+        void beginTransaction(SPISettings settings);
+        uint8_t transfer(uint8_t data);
+        uint16_t transfer16(uint16_t data);
+        void write(uint8_t data);
+        void write16(uint16_t data);
+        void write16(uint16_t data, bool msb);
+        void write32(uint32_t data);
+        void write32(uint32_t data, bool msb);
+        void writeBytes(uint8_t * data, uint32_t size);
+        void writePattern(uint8_t * data, uint8_t size, uint32_t repeat);
+        void transferBytes(uint8_t * out, uint8_t * in, uint32_t size);
+        void endTransaction(void);
+    private:
+        bool useHwCs;
+        uint8_t pinSet;
+        void writeBytes_(uint8_t * data, uint8_t size);
+        void transferBytes_(uint8_t * out, uint8_t * in, uint8_t size);
+        inline void setDataBits(uint16_t bits);
 };
 
-extern SPIImpl SPI;
+#if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_SPI)
+extern SPIClass SPI;
+#endif
 
-/* no_extern_c_check */
-
-#endif /* CS_FW_SRC_ARDUINO_SPI_H_ */
+#endif
